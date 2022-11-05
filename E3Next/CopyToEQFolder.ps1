e8fb513651a1329fdf91d29f75d9d8dd2d3a3199
Adam Liverman <livermana@gmail.com>
2022-11-05 08:23:58 -0400

    updated to only copy dll and exe's to folders

--- E3Next/CopyToEQFolder.ps1
+++ E3Next/CopyToEQFolder.ps1
@@ -4,7 +4,8 @@ if($Env:E3BuildDest)
     $ValidPath = Test-Path -Path "$Env:E3BuildDest"
     if($ValidPath)
     {
-        Copy-Item -Path "*.*" -Destination "$Env:E3BuildDest"
+        Copy-Item -Path "*.dll" -Destination "$Env:E3BuildDest"
+        Copy-Item -Path "*.exe" -Destination "$Env:E3BuildDest"
     }
 
     
