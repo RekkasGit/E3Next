b301ebf0dd48cafbfa8ca47d00867c60b0018ddd
Adam Liverman <livermana@gmail.com>
2024-01-29 18:57:49 -0500

    spelling

--- E3Next/Server/SharedDataClient.cs
+++ E3Next/Server/SharedDataClient.cs
@@ -529,7 +529,7 @@ namespace E3Core.Server
 			~OnCommandData()
 			{
 				//DO NOT CALL DISPOSE FROM THE FINALIZER! This should only ever be used in using statements
-				//if this is called, it will cause the domain to hang in the GC when shuttind down
+				//if this is called, it will cause the domain to hang in the GC when shutting down
 				//This is only here to warn you
 
 			}
