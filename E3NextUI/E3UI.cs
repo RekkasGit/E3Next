06e86bb314dd205523f3f04c5d9f0aab9781c1e7
Adam Liverman <livermana@gmail.com>
2024-04-07 15:00:53 -0400

    bumping version

--- E3NextUI/E3UI.cs
+++ E3NextUI/E3UI.cs
@@ -30,7 +30,7 @@ namespace E3NextUI
 		public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
 		[DllImportAttribute("user32.dll")]
 		public static extern bool ReleaseCapture();
-		public static string Version = "v1.2";
+		public static string Version = "v1.3";
         public static System.Diagnostics.Stopwatch _stopWatch = new System.Diagnostics.Stopwatch();
         public static volatile bool ShouldProcess = true;
 
