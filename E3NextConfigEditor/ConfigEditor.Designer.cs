5ee0048fbbb84391b2c4ed21099befe9d27cb29f
Adam Liverman <livermana@gmail.com>
2024-04-25 08:44:28 -0400

    fix krypton links

--- E3NextConfigEditor/ConfigEditor.Designer.cs
+++ E3NextConfigEditor/ConfigEditor.Designer.cs
@@ -28,36 +28,20 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.sectionListBox = new ComponentFactory.Krypton.Toolkit.KryptonListBox();
 			this.SuspendLayout();
 			// 
-			// sectionListBox
-			// 
-			this.sectionListBox.Items.AddRange(new object[] {
-            "Nukes",
-            "Heals",
-            "Burns"});
-			this.sectionListBox.ItemStyle = ComponentFactory.Krypton.Toolkit.ButtonStyle.Standalone;
-			this.sectionListBox.Location = new System.Drawing.Point(13, 13);
-			this.sectionListBox.Name = "sectionListBox";
-			this.sectionListBox.Size = new System.Drawing.Size(131, 425);
-			this.sectionListBox.TabIndex = 0;
-			// 
-			// Form1
+			// ConfigEditor
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.sectionListBox);
-			this.Name = "Form1";
+			this.Name = "ConfigEditor";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private ComponentFactory.Krypton.Toolkit.KryptonListBox sectionListBox;
 	}
 }
 
