3e42fb0f0319a9e076413b03de695a2061283b88
Adam Liverman <livermana@gmail.com>
2026-06-14 14:12:52 -0400

    show release notes in systray and bump version

---
+++ E3NextSysTray/Util/NotepadHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace E3NextSysTray.Util
+{
+	public static class NotepadHelper
+	{
+		[DllImport("user32.dll", EntryPoint = "SetWindowText")]
+		private static extern int SetWindowText(IntPtr hWnd, string text);
+
+		[DllImport("user32.dll", EntryPoint = "FindWindowEx")]
+		private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+		[DllImport("User32.dll", EntryPoint = "SendMessage")]
+		private static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+
+		public static void ShowMessage(string message = null, string title = null)
+		{
+			var startInfo = new ProcessStartInfo("notepad.exe");
+			startInfo.UseShellExecute = false;
+			Process notepad = Process.Start(startInfo);
+			if (notepad != null)
+			{
+				notepad.WaitForInputIdle();
+
+				if (!string.IsNullOrEmpty(title))
+				{
+					SetWindowText(notepad.MainWindowHandle, title);
+				}
+
+				if (!string.IsNullOrEmpty(message))
+				{
+					IntPtr child = FindWindowEx(notepad.MainWindowHandle, new IntPtr(0), "Edit", null);
+					SendMessage(child, 0x000C, 0, message);
+				}
+			}
+		}
+	}
+}
